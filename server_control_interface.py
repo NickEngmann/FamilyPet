1276118b21a1325f50e28e2079ee4a8ae8ba4ef0
Nick Engmann <NickEngmann@users.noreply.github.com>
2017-10-17 23:02:27 -0700

    using firebase as my https server

--- server_control_interface.py
+++ server_control_interface.py
@@ -1,23 +1,14 @@
 #! /usr/bin/python3
 
-import visa
-import time
-import re
+from firebase import firebase
 from time import gmtime, strftime, localtime
-import requests
-import socket
 
+# initialize Firebase Application connection
+class Atom(self):
+	def __init__(self):
+		self._fb = firebase.FirebaseApplication("https://atom-pet.firebase.io.com", None)
 
-# Get the IP address
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("gmail.com",80))
-ipAddress = s.getsockname()[0] #get the Ip address of your server (localhost)
-print(ipAddress)
-s.close()
-
-while(1):
-	#sends the request to the server to be time sync'd and compared
-	current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-	r = requests.post('http://'+ipAddress+':3000/pi', data= {'timestamp':})
-	print (r.status_code, r.reason)
-	time.sleep(1.0)
+		# Initial State
+		time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		data = {'state':'initialize', 'location':None, 'timestamp':time ,'user':None, 'command': None}
+		result = self._fb.patch("/status", data)
