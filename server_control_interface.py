b955e7e0818ca4ea9889bf1a6d64cd4eddba6ec0
NickEngmann <cyengmann@gmail.com>
2017-10-25 22:19:31 +0000

    changes to control interface

--- server_control_interface.py
+++ server_control_interface.py
@@ -6,7 +6,7 @@ from time import gmtime, strftime, localtime
 # initialize Firebase Application connection
 class Atom():
 	def __init__(self):
-		self._fb = firebase.FirebaseApplication("https://atom-pet.firebase.io.com", None)
+		self._fb = firebase.FirebaseApplication("https://atom-pet.firebaseio.com", None)
 
 		# Initial State
 		time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
